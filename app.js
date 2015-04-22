9eb72abd5fc11029a1915c3623559265c2073a82
Andrew Shatnyy <AndrewShatnyy@gmail.com>
2015-04-22 13:29:16 -0700

    update default

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-
 (function (argument) {
   'use strict';
   require('codemirror/mode/htmlembedded/htmlembedded');
@@ -14,11 +13,23 @@
 
   require('./lib/moboom-hint')(CodeMirror);
 
+
+
+  var getHtml = function() {
+    var ser = new XMLSerializer();
+    return ser.serializeToString(document);
+  };
+
+
+
   var codeMirror  = new CodeMirror(document.querySelector('#inspector'), {
     mode:'htmlembedded',
     lineNumbers: true,
-    extraKeys: {'Ctrl-Space':'autocomplete'}
+    extraKeys: {'Ctrl-Space':'autocomplete'},
+    value: getHtml()
   });
+
+
 
   window.codeMirror = codeMirror;
 
